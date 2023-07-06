9717d612f02a09ff06a2979e9ded4f4159cd6762
Kashyap Rathod <kashyaprathod94@gmail.com>
2023-07-07 00:08:17 +0530

    Implementing edit function

--- src/Components/CommentHeader.js
+++ src/Components/CommentHeader.js
@@ -4,7 +4,7 @@ import deleteIcon from '../assets/icons/icon-delete.svg';
 import editIcon from '../assets/icons/icon-edit.svg';
 import IconBtn from './IconBtn';
 
-const CommentHeader = ({id, user, createdAt, deleteComment}) => {
+const CommentHeader = ({id, user, createdAt, deleteComment, setEditing}) => {
   return (
     <div className='flex flex-row justify-between'>
         <div className='flex flex-row items-center'>
@@ -16,7 +16,7 @@ const CommentHeader = ({id, user, createdAt, deleteComment}) => {
           {
             user.username === "juliusomo" ? <div className='flex flex-row items-center space-x-4'>
               <button className='flex flex-row  items-center' onClick={() => deleteComment(id)}><img src={deleteIcon} alt="" className='mr-2'/><span className='text-rose-700 font-medium'>Delete</span></button>
-              <button className='flex flex-row  items-center' onClick={() => console.log("")}><img src={editIcon} alt="" className='mr-2'/><span className='text-violet-900 font-medium'>Edit</span></button>
+              <button className='flex flex-row  items-center' onClick={() => setEditing(true)}><img src={editIcon} alt="" className='mr-2'/><span className='text-violet-900 font-medium'>Edit</span></button>
               </div> : 
               <button className='flex flex-row  items-center'><img src={replyIcon} alt="" className='mr-2'/><span className='text-violet-900 font-medium'>Reply</span>
               </button> 
--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CommentHeader from './CommentHeader';
 import CommentList from './CommentList';
 
 
 const Comments = ({id, user, createdAt, deleteComment, content, replies}) => {
+  const [commentData, setCommentData] = useState(content);
+  const [editing, setEditing] = useState(false);
+
+
   return (
     <>
     <div className='bg-white w-2/4 p-3 mb-2 rounded-xl'>
-        <CommentHeader user={user} createdAt={createdAt} id={id} deleteComment={deleteComment}/>
+        <CommentHeader user={user} createdAt={createdAt} id={id} deleteComment={deleteComment} setEditing={setEditing}/>
         <div className="w-11/12 pt-3 pb-3">
-            <span className="text-slate-700">{content}</span>
+            {!editing ? (
+                <span className="text-slate-700">{content}</span>
+            ) : 
+            <textarea className='w-full p-2 h-2/5 rounded-7 border-2 rounded-md border-blue-gray-300' onChange={e => setCommentData(e.target.value)} value={commentData}></textarea>
+            }
+            {editing && <button className='text-white font-medium bg-violet-900 p-2 rounded-md'>UPDATE</button>}
         </div>
     </div>
     {
