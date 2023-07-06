40440b79897aa8225c7852a8a4b14abf3f9f204a
Kashyap Rathod <kashyaprathod94@gmail.com>
2023-07-06 22:55:16 +0530

    Fixed recursive comments error

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -5,18 +5,19 @@ import CommentList from './CommentList';
 
 const Comments = ({id, user, createdAt, deleteComment, content, replies}) => {
   return (
+    <>
     <div className='bg-white w-2/4 p-3 mb-2 rounded-xl'>
         <CommentHeader user={user} createdAt={createdAt} id={id} deleteComment={deleteComment}/>
         <div className="w-11/12 pt-3 pb-3">
             <span className="text-slate-700">{content}</span>
         </div>
-        {
-            replies?.length > 0 && (
-                <CommentList comments={replies}/>
-            )
-        }
     </div>
+    {
+        replies?.length > 0 && (
+            <CommentList comments={replies}/>
+        )
+    }
+    </>
   )
 }
-
 export default Comments
