35bf941a37bddb99cb510dcc99f318cf9b612fce
shawn <shawnpereiraswp@gmail.com>
2023-07-09 00:43:13 +0530

    added reply and fixed bugs

--- src/Components/CommentHeader.js
+++ src/Components/CommentHeader.js
@@ -1,29 +1,155 @@
-import React from 'react';
-import replyIcon from '../assets/icons/icon-reply.svg';
-import deleteIcon from '../assets/icons/icon-delete.svg';
-import editIcon from '../assets/icons/icon-edit.svg';
-import IconBtn from './IconBtn';
+import React, { useState } from "react";
+import replyIcon from "../assets/icons/icon-reply.svg";
+import deleteIcon from "../assets/icons/icon-delete.svg";
+import editIcon from "../assets/icons/icon-edit.svg";
+import IconBtn from "./IconBtn";
+import { useLocalStorage } from "usehooks-ts";
 
-const CommentHeader = ({id, user, createdAt, deleteComment, setEditing}) => {
+const CommentHeader = ({ id, user, createdAt, deleteComment, setEditing }) => {
+  const [replying, setReplying] = useState(false);
+  const [replyText, setReplyText] = useLocalStorage("replyText", "");
+  const [replies, setReplies] = useState([]);
+
+  const respond = () => {
+    setReplying(true);
+  };
+
+  const handleReplyChange = (event) => {
+    setReplyText(event.target.value);
+  };
+
+  const handleReplySubmit = () => {
+    if (replyText.trim() !== "") {
+      const newReply = {
+        id: generateId(),
+        user: user.username,
+        createdAt: new Date().toISOString(),
+        content: replyText,
+        replies: [],
+      };
+
+      setReplies((prevReplies) => [newReply, ...prevReplies]);
+      setReplyText("");
+      setReplying(false);
+    }
+  };
+
+  const generateId = () => {
+    return Math.random().toString(36).substring(2, 7);
+  };
+
+  const timeSinceCreated = (createdAt) => {
+    const createdTime = new Date(createdAt);
+    const currentTime = new Date();
+    const timeDiff = Math.round((currentTime - createdTime) / 1000); // Time difference in seconds
+
+    if (timeDiff < 60) {
+      return `${timeDiff} seconds${timeDiff !== 1 ? "s" : ""} ago`;
+    } else if (timeDiff < 3600) {
+      const minutes = Math.floor(timeDiff / 60);
+      return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+    } else if (timeDiff < 86400) {
+      const hours = Math.floor(timeDiff / 3600);
+      return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+    } else {
+      const days = Math.floor(timeDiff / 86400);
+      return `${days} day${days !== 1 ? "s" : ""} ago`;
+    }
+  };
+
+  const handleDeleteReply = (replyId) => {
+    const updatedReplies = replies.filter((reply) => reply.id !== replyId);
+    setReplies(updatedReplies);
+  };
+
+  const renderReplies = (replies) => {
+    return (
+      <div className="ml-6 mt-2 ">
+        {replies.map((reply) => (
+          <div key={reply.id} className="ml-6 mt-2">
+            <div className="flex flex-row items-center">
+              <img src="./images/image-juliusomo.png" alt="-" width={30} />
+              <span className="font-medium mr-2 ml-2">juliusomo</span>
+              <span className="font-normal text-slate-400">
+                {timeSinceCreated(reply.createdAt)}
+              </span>
+            </div>
+            <div className="bg-gray-100 px-4 py-2 rounded-lg">
+              <p>{reply.content}</p>
+              <div className="flex justify-end mt-2">
+                {user.username === reply.user && (
+                  <button
+                    className="text-rose-700 font-medium"
+                    onClick={() => handleDeleteReply(reply.id)}
+                  >
+                    Delete
+                  </button>
+                )}
+              </div>
+              {reply.replies &&
+                reply.replies.length > 0 &&
+                renderReplies(reply.replies)}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div className='flex flex-row justify-between'>
-        <div className='flex flex-row items-center'>
-            <img src={user.image.png} alt="." className='mr-2' width={30}/>
-            <span className='font-medium mr-2'>{user.username}</span>
-            <span className='font-normal text-slate-400'>{createdAt}</span>
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center">
+          <img src={user.image.png} alt="." className="mr-2" width={30} />
+          <span className="font-medium mr-2">{user.username}</span>
+          <span className="font-normal text-slate-400">1 week ago</span>
         </div>
-        <div className='flex flex-row'>
-          {
-            user.username === "juliusomo" ? <div className='flex flex-row items-center space-x-4'>
-              <button className='flex flex-row  items-center' onClick={() => deleteComment(id)}><img src={deleteIcon} alt="" className='mr-2'/><span className='text-rose-700 font-medium'>Delete</span></button>
-              <button className='flex flex-row  items-center' onClick={() => setEditing(true)}><img src={editIcon} alt="" className='mr-2'/><span className='text-violet-900 font-medium'>Edit</span></button>
-              </div> : 
-              <button className='flex flex-row  items-center'><img src={replyIcon} alt="" className='mr-2'/><span className='text-violet-900 font-medium'>Reply</span>
-              </button> 
-          }         
+        <div className="flex flex-row">
+          {user.username === "juliusomo" ? (
+            <div className="flex flex-row items-center space-x-4">
+              <button
+                className="flex flex-row items-center"
+                onClick={() => deleteComment(id)}
+              >
+                <img src={deleteIcon} alt="" className="mr-2" />
+                <span className="text-rose-700 font-medium">Delete</span>
+              </button>
+              <button
+                className="flex flex-row items-center"
+                onClick={() => setEditing(true)}
+              >
+                <img src={editIcon} alt="" className="mr-2" />
+                <span className="text-violet-900 font-medium">Edit</span>
+              </button>
+            </div>
+          ) : (
+            <button className="flex flex-row items-center" onClick={respond}>
+              <img src={replyIcon} alt="" className="mr-2" />
+              <span className="text-violet-900 font-medium">Reply</span>
+            </button>
+          )}
         </div>
+      </div>
+      {replying && (
+        <div className="flex flex-row items-center mt-2">
+          <input
+            type="text"
+            value={replyText}
+            onChange={handleReplyChange}
+            placeholder="Write a reply..."
+            className="border border-gray-300 rounded-lg px-2 py-1 w-full h-14 mr-2 "
+          />
+          <button
+            className="bg-violet-700 text-white px-5 py-2 rounded-lg"
+            onClick={handleReplySubmit}
+          >
+            Reply
+          </button>
+        </div>
+      )}
+      {replies.length > 0 && renderReplies(replies)}
     </div>
-  )
-}
+  );
+};
 
-export default CommentHeader
+export default CommentHeader;
--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
-import CommentHeader from './CommentHeader';
-import CommentList from './CommentList';
+import React, { useState } from "react";
+import CommentHeader from "./CommentHeader";
+import CommentList from "./CommentList";
 
-
-const Comments = ({id, user, createdAt, deleteComment, content, replies, editComment}) => {
+const Comments = ({
+  id,
+  user,
+  createdAt,
+  deleteComment,
+  content,
+  replies,
+  editComment,
+}) => {
   const [commentData, setCommentData] = useState(content);
   const [editing, setEditing] = useState(false);
 
   const updateComment = () => {
     editComment(commentData, id);
     setEditing(false);
-  }
-
+  };
 
   return (
     <>
-    <div className='bg-white w-2/4 p-3 mb-2 rounded-xl'>
-        <CommentHeader user={user} createdAt={createdAt} id={id} deleteComment={deleteComment} setEditing={setEditing}/>
-        <div className="w-11/12 pt-3 pb-3">
-            {!editing ? (
-                <span className="text-slate-700">{content}</span>
-            ) : 
-            <textarea className='w-full p-2 h-2/5 rounded-7 border-2 rounded-md border-blue-gray-300' onChange={e => setCommentData(e.target.value)} value={commentData}></textarea>
-            }
-            {editing && <button className='text-white font-medium bg-violet-900 p-2 rounded-md' onClick={updateComment}>UPDATE</button>}
+      <div className="bg-white w-2/4 p-3 mb-2 rounded-xl flex flex-col">
+        <div className="">
+          <CommentHeader
+            user={user}
+            createdAt={createdAt}
+            id={id}
+            deleteComment={deleteComment}
+            setEditing={setEditing}
+          />
         </div>
-    </div>
-    {
-        replies?.length > 0 && (
-            <CommentList comments={replies}/>
-        )
-    }
+        <div className="w-11/12 pt-3 pb-3">
+          {!editing ? (
+            <span className="text-slate-700">{content}</span>
+          ) : (
+            <textarea
+              className="w-full p-2 h-2/5 rounded-7 border-2 rounded-md border-blue-gray-300"
+              onChange={(e) => setCommentData(e.target.value)}
+              value={commentData}
+            ></textarea>
+          )}
+          {editing && (
+            <button
+              className="text-white font-medium bg-violet-900 p-2 rounded-md"
+              onClick={updateComment}
+            >
+              UPDATE
+            </button>
+          )}
+        </div>
+      </div>
+      {replies?.length > 0 && <CommentList comments={replies} />}
     </>
-  )
-}
-export default Comments
+  );
+};
+export default Comments;
